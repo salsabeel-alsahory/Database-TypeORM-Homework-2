152f44016eff3cc5a569becda6f00b7f05b0cadd
Salsabeel Mohammad Hassan Alsahory <202006388@bethlehem.edu>
2023-09-28 21:22:32 +0300

    adding on the tsconfig

--- index.ts
+++ index.ts
@@ -1,31 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-// const cors = require('cors');
-const dataSource = require('./typeorm'); // Import your TypeORM configuration
-// const routes = require('./routes');
-
-dotenv.config();
+import express from 'express';
+import { createConnection } from 'typeorm';
+import authRoutes from './routes/authRoutes';
+import { validateRegistration } from './middleware/registerValidation';
 
 const app = express();
+app.use(express.json());
 
-// Middleware
-// app.use(cors());
-app.use(bodyParser.json());
-
-// Use the TypeORM connection
-dataSource.then(() => {
+// Set up database connection
+createConnection().then(() => {
   console.log('Connected to the database');
-}).catch((error: any) => {
-  console.error('Failed to connect to the database:', error);
-  process.exit(1);
+}).catch((error) => {
+  console.error('Database connection error:', error);
 });
 
-// Routes
-// app.use('/', routes);
+// Use validation middleware
+app.use(validateRegistration);
+// Use routes
+app.use('/auth', authRoutes);
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
